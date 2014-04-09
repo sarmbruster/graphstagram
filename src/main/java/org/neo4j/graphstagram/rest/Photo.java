97e6d9449badc4039330fb1a7047a28c9da56f83
Stefan Armbruster <stefan@armbruster-it.de>
2014-04-10 00:10:40 +0200

    creating and retrieving photos for friends of level X

--- src/main/java/org/neo4j/graphstagram/rest/Photo.java
+++ src/main/java/org/neo4j/graphstagram/rest/Photo.java
@@ -1,19 +1,26 @@
 package org.neo4j.graphstagram.rest;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.jpeg.JpegDirectory;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.index.Index;
 import org.neo4j.helpers.collection.IteratorUtil;
 import org.neo4j.helpers.collection.MapUtil;
 import org.neo4j.server.database.CypherExecutor;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import java.util.Iterator;
-import java.util.Map;
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
 
 /**
  * provides a REST interface for managin photos.
@@ -22,32 +29,88 @@ import java.util.Map;
 public class Photo
 {
 
+    // where we store images
+    static final String FILE_STORAGE = "filestorage";
+
     @Context
     protected GraphDatabaseService graphDatabaseService;
 
     @Context
     protected CypherExecutor cypherExecutor;
 
-/*
+    @PUT
+    public long addPhoto(@QueryParam("name") String name, Map<String, Object> payload) throws ImageProcessingException, IOException, MetadataException {
+
+        byte[] raw = DatatypeConverter.parseBase64Binary((String) payload.get("raw"));
+        String fileName = UUID.randomUUID().toString();
+        File imageFile = new File(FILE_STORAGE, fileName);
+
+        // write image to secondary datastore (filesystem)
+        FileUtils.writeByteArrayToFile(imageFile, raw);
+
+        // extract some metadata
+        BufferedInputStream stream = new BufferedInputStream(new ByteArrayInputStream(raw));
+
+        Metadata metadata = ImageMetadataReader.readMetadata(stream, true);
+        JpegDirectory directory = metadata.getDirectory(JpegDirectory.class);
+
+        // build map for metadata to be stored in graph
+        Map<String, Object> params = MapUtil.genericMap(
+                "width", directory.getImageWidth(),
+                "height", directory.getImageHeight(),
+                "url", imageFile.toURI().toURL().toString()
+        );
+
+        // write metadata to the graph
+        ExecutionResult result = cypherExecutor.getExecutionEngine()
+                .execute("MATCH (user:User {name:{name}}) CREATE (user)-[:SHOOT_PHOTO]->(photo:Photo {params}) return id(photo) as id",
+                        MapUtil.<String, Object>genericMap("name", name, "params", params));
+        return (long) IteratorUtil.single(result.columnAs("id"));
+    }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Iterator<Object> getUsernames(
-            @DefaultValue("0") @QueryParam("skip") int skip,
-            @DefaultValue("20") @QueryParam("limit") int limit) {
-        Map<String,Object> params = MapUtil.<String,Object>genericMap("skip", skip, "limit", limit);
-        ExecutionResult result =  cypherExecutor.getExecutionEngine()
-                .execute("MATCH (user:User) RETURN user.name SKIP {skip} LIMIT {limit}", params);
-        return result.columnAs("user.name");
-    }
-*/
+    @Path("{userName}/friend")
+    public Map<String, Collection<Map<String, Object>>> getPhotosOfFriendsWithLevel(
+            @PathParam("userName") String userName,
+            @QueryParam("level") @DefaultValue("1") int level
+    ) throws IOException {
 
-    @PUT
-    public long addPhoto(@QueryParam("name") String name, Map<String, Object> payload) {
-        ExecutionResult result = cypherExecutor.getExecutionEngine()
-                .execute("CREATE (user:User {name:{name}}) return id(user) as id",
-                        MapUtil.<String, Object>genericMap("name", name));
-        return (long) IteratorUtil.single(result.columnAs("id"));
+        try (Transaction tx = graphDatabaseService.beginTx()) {
+            StringBuilder cypher = new StringBuilder();
+            cypher.append("MATCH (:User {name: {userName}})-[:FRIEND*").append(level).append("]-(friend)-[:SHOOT_PHOTO]->(photo) RETURN friend.name, photo");
+            ExecutionResult result = cypherExecutor.getExecutionEngine().execute(cypher.toString(), Collections.<String, Object>singletonMap("userName", userName));
+
+            Map<String,Collection<Map<String,Object>>> retVal = new HashMap<>();
+
+            for (Map<String,Object> row: result) {
+                String friend = (String) row.get("friend.name");
+
+                Collection<Map<String,Object>> photos = retVal.get(friend);
+                if (photos==null) {
+                    photos = new ArrayList<>();
+                    retVal.put(friend, photos);
+                }
+
+                Map<String,Object> photoData = new HashMap<>();
+
+                Node photo = (Node) row.get("photo");
+                for (String key: photo.getPropertyKeys()) {
+                    Object value = photo.getProperty(key);
+                    if (key.equals("url")) {
+                        // amend graph search result with contents from secondary datasource (filesystem)
+                        InputStream is = new URL(value.toString()).openStream();
+                        byte[] raw = IOUtils.toByteArray(is);
+                        photoData.put(key, DatatypeConverter.printBase64Binary(raw));
+                    } else {
+                        photoData.put(key, value);
+                    }
+
+                }
+                photos.add(photoData);
+            }
+            tx.success();
+            return retVal;
+        }
     }
 
     /**
--- src/test/groovy/org/neo4j/graphstagram/rest/PhotoSpec.groovy
+++ src/test/groovy/org/neo4j/graphstagram/rest/PhotoSpec.groovy
@@ -1,8 +1,14 @@
 package org.neo4j.graphstagram.rest
 
+import com.drew.imaging.ImageMetadataReader
+import com.drew.metadata.Directory
+import com.drew.metadata.jpeg.JpegDirectory
 import org.junit.ClassRule
 import org.neo4j.extension.spock.Neo4jServerResource
-import org.neo4j.helpers.collection.IteratorUtil
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.Transaction
+import org.neo4j.visualization.graphviz.GraphvizWriter
+import org.neo4j.walk.Walker
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -21,13 +27,45 @@ class PhotoSpec extends Specification {
 
 
     def "should add new picture for existing user"() {
-        when:
-        def response = neo4j.http.PUT("db/photo?name=Sam",
-                [raw: getClass().classLoader.getResourceAsStream("darthvader.jpg").bytes ])
+        setup: "create a user"
+        neo4j.http.PUT("db/user?name=Sam")
+        neo4j.http.PUT("db/user?name=Joe")
+
+        // make 'em friends
+        neo4j.http.PUT("db/user/Sam/friend/Joe")
+
+        when: "add a photo"
+        def rawImageData = getClass().classLoader.getResourceAsStream("darthvader.jpg").bytes
+        def response = neo4j.http.PUT("db/photo?name=Sam", [raw: rawImageData])
 
         then:
         response.status()==200
-//        "START n=node({id}) RETURN n.name AS name".cypher([id:nodeId])[0]['name'] == 'Sam'
+
+        when: "check if photo has been created"
+        def result = "MATCH (:User {name:{name}})-[:SHOOT_PHOTO]->(photo) RETURN photo".cypher([name:'Sam'])
+
+        then:
+        result[0]["photo"]
+
+        when:
+        response = neo4j.http.GET("db/photo/Joe/friend")
+
+        then:
+        response.status()==200
+        response.rawContent() == "abc"
+
+    }
+
+    def "processing of image metadata"() {
+
+        when:
+        def m = ImageMetadataReader.readMetadata(new BufferedInputStream(getClass().classLoader.getResourceAsStream("darthvader.jpg")), true)
+        Directory dir = m.getDirectory(JpegDirectory.class)
+
+        then:
+        dir.imageHeight == 126
+        dir.imageWidth == 126
+
     }
 
 /*
@@ -51,4 +89,21 @@ class PhotoSpec extends Specification {
         "CREATE (:User {name:{name}})".cypher name:userName
     }
 */
+
+    def dumpGraphToSvg() {
+        Transaction tx = neo4j.graphDatabaseService.beginTx()
+        try {
+            File dotFile = File.createTempFile("temp", ".dot")
+            File svgFile = File.createTempFile("temp", ".svg")
+            new GraphvizWriter().emit(dotFile, Walker.fullGraph(neo4j.graphDatabaseService))
+            def proc = "/usr/bin/dot -Tsvg ${dotFile.absolutePath}".execute()
+            svgFile.withWriter { Writer it -> it << proc.in.text }
+            dotFile.delete()
+            println svgFile.absolutePath
+            tx.success()
+        } finally {
+            tx.close()
+        }
+    }
+
 }
